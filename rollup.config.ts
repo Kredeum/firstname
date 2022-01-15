202948f72297a5e643af2ac0a4dccb1284896ca6
zapaz.eth <alain@kredeum.com>
2022-01-15 10:55:20 +0100

    first test

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,12 @@
 import svelte from "rollup-plugin-svelte";
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
-import json from "@rollup/plugin-json";
-import builtins from "rollup-plugin-node-builtins";
+import css from "rollup-plugin-css-only";
 
-import autoPreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
+import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -32,58 +31,54 @@ function serve() {
   };
 }
 
-const toRollupConfig = (component) => {
-  return {
-    input: `svelte/${component}.svelte`,
-    output: {
-      sourcemap: false,
-      format: "iife",
-      name: "app",
-      file: `docs/build/${component}.js`
-    },
-    plugins: [
-      svelte({
-        preprocess: autoPreprocess(),
-        compilerOptions: {
-          customElement: true,
-          // enable run-time checks when not in production
-          dev: !production
-        }
-        // we'll extract any component CSS out into
-        // a separate file - better for performance
-      }),
-      typescript({ sourceMap: !production }),
+export default {
+  input: "./svelte/main.ts",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "dapp/build/bundle.js"
+  },
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess({ sourceMap: !production }),
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production
+      }
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css({ output: "bundle.css" }),
 
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
-      builtins(),
-      resolve({
-        browser: true,
-        dedupe: ["svelte"]
-      }),
-      commonjs(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ["svelte"]
+    }),
+    commonjs(),
+    typescript({
+      sourceMap: !production,
+      inlineSources: !production
+    }),
 
-      // In dev mode, call `npm run start` once
-      // the bundle has been generated
-      !production && serve(),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
-      // Watch the `public` directory and refresh the
-      // browser on changes when not in production
-      !production && livereload("docs"),
+    // Watch the `dapp` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("dapp"),
 
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      production && terser(),
-
-      json()
-    ],
-    watch: {
-      clearScreen: false
-    }
-  };
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
-
-export default [toRollupConfig("App")];

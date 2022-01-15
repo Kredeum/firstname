5542e7685b189273f4381e4b47bd9b0c804f683d
zapaz.eth <alain@kredeum.com>
2022-01-15 08:07:16 +0100

    clone Template and rename FIrstName

---
+++ deploy/FirstName.deploy.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const deployFirstName = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const deployResult = await deploy("FirstName", {
+    from: deployer,
+    args: [],
+    log: true
+  });
+
+  if (deployResult.newlyDeployed) {
+    console.log("FirstName newly deployed");
+  }
+};
+deployFirstName.tags = ["FirstName"];
+
+export default deployFirstName;
